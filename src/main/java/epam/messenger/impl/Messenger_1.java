b869ffdb34d07342484f1d60b15fd448e77dd39c
sasha170261 <54392757+sasha170261@users.noreply.github.com>
2021-07-30 18:31:02 +0300

    Add files via upload

---
+++ src/main/java/epam/messenger/impl/Messenger_1.java
@@ -0,0 +1,20 @@
+package epam.messenger.impl;
+
+import epam.messenger.MesssengerService;
+
+/**
+ * class Messenger_1
+ */
+public class Messenger_1 implements MesssengerService
+{
+  /**
+   * Send message
+   * 
+   * @param msg   message to be send;
+   */
+  public void send(String msg)
+  {
+    System.out.println("The message \" + msg + \" is sent by <" + this.getClass().getName() + ">");
+  }
+
+}
