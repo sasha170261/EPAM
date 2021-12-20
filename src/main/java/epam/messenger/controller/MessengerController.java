c45fdbda22791722ae3283a9cc3bc6857f89e2b1
ashap <ashaposhnikov@hotmail.com>
2021-12-20 19:28:58 +0200

    Update-1

--- src/main/java/epam/messenger/controller/MessengerController.java
+++ src/main/java/epam/messenger/controller/MessengerController.java
@@ -39,7 +39,7 @@ public class MessengerController
    * @param request
    * @return ResponseEntity
    * 
-   * test-update
+   * test-update-1
    */
   @PostMapping(value = "/messengers/send")
   public ResponseEntity<String> sendMessage(@RequestBody SendRequest request)
